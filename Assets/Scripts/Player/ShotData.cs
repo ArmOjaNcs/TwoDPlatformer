a856468261758089f64173073a2ad17256d690ac
ArmOjaNcs <tverarmen@gmail.com>
2024-11-30 23:08:31 +0300

    Update3

--- Assets/Scripts/Player/ShotData.cs
+++ Assets/Scripts/Player/ShotData.cs
@@ -1,13 +1,13 @@
 public struct ShotData
 {
-    public bool IsShooting { get; private set; }
-    public bool IsRightDirection {  get; private set; }
-    public bool IsDucking { get; private set; }
-
     public ShotData(bool  isShooting, bool isRightDirection, bool isDucking)
     {
         IsShooting = isShooting;
         IsRightDirection = isRightDirection;
         IsDucking = isDucking;
     }
+
+    public bool IsShooting { get; private set; }
+    public bool IsRightDirection {  get; private set; }
+    public bool IsDucking { get; private set; }
 }
